adef25a99e8d59ef102a34b67190c2dba3091c85
Miguel <i1720539@continental.edu.pe>
2021-03-03 22:57:20 -0500

    Inicializacion con Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:shopping_list/MainScreens/homeScreen.dart';
 import 'package:shopping_list/MainScreens/login/welcomeScreen.dart';
@@ -6,6 +7,7 @@ import 'package:shopping_list/Utils/ConstantsApp.dart';
 import 'package:shopping_list/Utils/TextApp.dart';
 import 'package:shopping_list/Widgets/Design/DesignWidgets.dart';
 import 'package:splashscreen/splashscreen.dart';
+import 'dart:developer' as developer;
 
 void main() {
   runApp(MyApp());
@@ -35,20 +37,39 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  //init
+  final Future<FirebaseApp> _initializationFirebase = Firebase.initializeApp();
+  static const String TAG = "Main";
   @override
   Widget build(BuildContext context) {
-    return SplashScreen(
-      seconds: ConstanstApp.TIME_SPLASH_SCREEN,
-      navigateAfterSeconds: WelcomeScreen(),
-      image: AssetsImages.ImageLogo(),
-      backgroundColor: Colors.white,
-      photoSize: 100,
-      loadingText: Text(
-        TextApp.LOADING,
-        style: Theme.of(context).textTheme.bodyText1,
-      ),
-      loaderColor: Theme.of(context).primaryColor,
-      gradientBackground: DesignWidgets.linearGradientMain(context),
+    return Center(
+      child: FutureBuilder(
+          future: _initializationFirebase,
+          builder: (context, snapshot) {
+            //checkforerrors
+            if (snapshot.hasError) {
+              developer.log(TAG + ", Firebase init. ERROR");
+              return SnackBar(content: Text("Error inicializando Firebase"));
+            } else if (snapshot.connectionState == ConnectionState.done) {
+              developer.log(TAG + ", Firebase init. DONE");
+
+              //done
+              return SplashScreen(
+                seconds: ConstanstApp.TIME_SPLASH_SCREEN,
+                navigateAfterSeconds: WelcomeScreen(),
+                image: AssetsImages.ImageLogo(),
+                backgroundColor: Colors.white,
+                photoSize: 100,
+                loadingText: Text(
+                  TextApp.LOADING,
+                  style: Theme.of(context).textTheme.bodyText1,
+                ),
+                loaderColor: Theme.of(context).primaryColor,
+                gradientBackground: DesignWidgets.linearGradientMain(context),
+              );
+            }
+            return CircularProgressIndicator();
+          }),
     );
   }
 }
